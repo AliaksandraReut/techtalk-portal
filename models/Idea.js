dcc5f194885c77ff1f89d6489a44ebd7a57eab76
AliaksandraReut <aliaksandra_reut@epam.com>
2014-04-23 19:08:25 +0300

    Redesign login form
    Redesign login form

--- models/Idea.js
+++ models/Idea.js
@@ -1,12 +1,12 @@
 var mg = require("mongoose"),
   Schema = mg.Schema,
   IdeaSchema = new Schema({
-    author:      { type: String, ref: 'User' },
-    ideaText:    { type: String },
-	comments:	 [ { type: String, ref: 'Comment'} ],
-    likes:       [ { type: String, ref: 'Like'} ],
-    created:     { type: Date, 'default': Date.now },
-    updated:     { type: Date, 'default': Date.now }
+    author:         { type: String, ref: 'User' },
+    ideaText:       { type: String },
+    comments:	    [ { type: String, ref: 'Comment'} ],
+    likes:          [ { type: String, ref: 'User'} ],
+    created:        { type: Date, 'default': Date.now },
+    updated:        { type: Date, 'default': Date.now }
   });
 
 exports.Idea = mg.model('Idea', IdeaSchema);
---
+++ scripts/services/ideas.js
@@ -0,0 +1,69 @@
+;(function(ng) {
+    'use strict';
+
+    ng.module('tp.services')
+        .factory('TT', ['$resource', function($resource){
+            return $resource('/portal/api/tt');
+        }])
+        .factory('Idea', ['$resource', function($resource){
+            return $resource('/portal/api/ideas/:ideaId', {ideaId: '@id'});
+        }])
+        .factory('Comment', ['$resource', function($resource){
+            return $resource('/portal/api/comment/:commentId');
+        }])
+        .factory('Like', ['$resource', function($resource){
+            return $resource('/portal/api/like');
+        }])
+        .service('ideaFactory', ['$rootScope', 'Idea', function($rootScope, Idea){
+            return {
+                getAll: function(){
+                    return Idea.query();
+                },
+                get: function(ideaId){
+                    var idea = Idea.get({ideaId: ideaId});
+                    console.log(idea);
+                    return idea;
+                },
+                post: function(ideaText){
+                    var idea = new Idea;
+                    idea.ideaText = ideaText;
+                    idea.author = $rootScope.global.currentUser._id;
+                    idea.$save();
+                    return idea;
+                }
+            };
+        }])
+        .service('commentFactory', ['$rootScope', 'Comment', function($rootScope, Comment){
+            return {
+                getAll: function(ideaId){
+                    return Comment.query({ideaId: ideaId});
+                },
+                post: function(ideaId, commentText){
+                    var comment = new Comment();
+                    comment.commentText = commentText;
+                    comment.idea = ideaId;
+                    console.log($rootScope.global.currentUser);
+                    comment.author = $rootScope.global.currentUser._id;
+                    comment.$save();
+                    return comment;
+                },
+                remove: function(comment){
+                    Comment.remove({commentId: comment._id});
+                }
+            };
+        }])
+        .service('likeFactory', ['Like', function(Like){
+            return {
+                post: function(ideaId){
+                    var like = new Like();
+                    like.ideaId = ideaId;
+                    like.$save();
+                    return like;
+                },
+                delete: function(ideaId){
+                    Like.remove({ideaId: ideaId});
+                }
+            };
+        }]);
+
+})(angular);
