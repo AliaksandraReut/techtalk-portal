5af7a1aaa3e68da2bb9bc9e729ae7a4dca6bf76b
Egor Miasnikov <e.miasnikov@gmail.com>
2014-05-04 12:23:02 +0300

    fixed appjs, index.html and create grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,98 +1,103 @@
 'use strict';
 
-module.exports = function(grunt) {
-  var config = {
-      app: './',
-      dist: 'dist'
+module.exports = function (grunt) {
+    var config = {
+        app: './',
+        dist: 'dist'
     };
 
-  try {
-    config.app = require('./bower.json').appPath || config.app;
-  } catch (e) {}  
-
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.initConfig({
-    config: config,
-    watch: {
-      options: {
-        livereload: true
-      },
-      scripts: {
-        files: ['scripts/{,**/}*.js'],
-        tasks: ['copy:development']
-      }
-    },
-    concat: {
-      development: {
-        files: {
-          'public/js/core.js': [
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/angular/angular.js',
-            'bower_components/angular-route/angular-route.js',
-            'bower_components/angular-resource/angular-resource.js',
-            'bower_components/angular-cookies/angular-cookies.js'
-          ]
-        }
-      },
-
-      production: {
-      }
-    },
-
-    copy: {
-      development: {
-        files: [
-          {
-            expand: true,
-            cwd: './scripts/',
-            src: '{,**/}*.js',
-            dest: './public/js/'
-          },
-          {
-            expand: true,
-            cwd: './files/user-photo/',
-            src: '*',
-            dest: './public/img/user/'
-          },
-          {
-            expand: true,
-            cwd: './styles/fonts/',
-            src: '**',
-            dest: './public/fonts/'
-          },
-          {
-            expand: true,
-            cwd: './styles/css/',
-            src: '**',
-            dest: './public/css/'
-          }
-        ]
-      },
-      build: {
-        files: {
-          'dist/': [
-            './public/{css/**, img/**}',
-            'views/**',
-            'server.js',
-            'node_modules/**'
-          ]
-        }
-      }
-    },
-
-    clean: {
-      dist: {
-        src: ['<%= config.dist %>', './.tmp'],
-        force: true
-      }
+    try {
+        config.app = require('./bower.json').appPath || config.app;
+    } catch (e) {
     }
-  });
 
-  grunt.registerTask('default', ['copy:development', 'concat:development']);
-  grunt.registerTask('build', ['clean:dist', 'copy:build', 'concat']);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.initConfig({
+        config: config,
+        watch: {
+            options: {
+                livereload: true
+            },
+            scripts: {
+                files: ['scripts/{,**/}*.js'],
+                tasks: ['copy:js']
+            },
+            css: {
+                files: ['styles/css/*.css'],
+                tasks: ['copy:css']
+            }
+        },
+        concat: {
+            development: {
+                files: {
+                    'public/js/core.js': [
+                        'bower_components/jquery/dist/jquery.js',
+                        'bower_components/angular/angular.js',
+                        'bower_components/angular-route/angular-route.js',
+                        'bower_components/angular-resource/angular-resource.js',
+                        'bower_components/angular-cookies/angular-cookies.js'
+                    ]
+                }
+            }
+        },
+
+        copy: {
+            css: {
+                expand: true,
+                cwd: './styles/css/',
+                src: '**',
+                dest: './public/css/'
+            },
+            js: {
+                expand: true,
+                cwd: './scripts/',
+                src: '{,**/}*.js',
+                dest: './public/js/'
+            },
+            photo: {
+                expand: true,
+                cwd: './files/user-photo/',
+                src: '*',
+                dest: './public/img/user/'
+            },
+            images: {
+                expand: true,
+                cwd: './images/',
+                src: '*',
+                dest: './public/images/'
+            },
+            font: {
+                expand: true,
+                cwd: './styles/fonts/',
+                src: '**',
+                dest: './public/fonts/'
+            },
+
+            build: {
+                files: {
+                    'dist/': [
+                        './public/{css/**, img/**}',
+                        'views/**',
+                        'server.js',
+                        'node_modules/**'
+                    ]
+                }
+            }
+        },
+
+        clean: {
+            dist: {
+                src: ['<%= config.dist %>', './.tmp'],
+                force: true
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['copy:css', 'copy:js', 'copy:font', 'copy:photo', 'copy:images', 'concat:development']);
+    grunt.registerTask('build', ['clean:dist', 'copy:build', 'concat']);
 };
